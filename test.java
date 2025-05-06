8d31c422b99fac85f34b9bb95453bd0c1635d60e
BhargavaGh <145017789+BhargavaGh@users.noreply.github.com>
2025-05-06 23:27:31 +0530

    test.java

---
+++ test.java
@@ -0,0 +1,20 @@
+public class JenkinsTest {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+        System.out.println("This is a test build.");
+        
+        int result = addNumbers(5, 7);
+        System.out.println("5 + 7 = " + result);
+        
+        if (result == 12) {
+            System.out.println("Test passed!");
+        } else {
+            System.out.println("Test failed!");
+        }
+    }
+    
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
